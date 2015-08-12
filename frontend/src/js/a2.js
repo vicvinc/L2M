b05a4b38effc8b6d8fee85600dd4f9ea45e03a9b
vicvinc <1285044294@qq.com>
2015-08-12 20:39:55 +0800

    dev

---
+++ frontend/src/js/a2.js
@@ -0,0 +1,16 @@
+/* 
+* @Author: Administrator
+* @Date:   2015-08-12 17:08:16
+* @Last Modified by:   Administrator
+* @Last Modified time: 2015-08-12 19:31:59
+*/
+
+'use strict';
+
+var people = [
+	{id: 1, name: "John"},
+	{id: 2, name: "Mary"}
+];
+
+//ajax and display a list of projects for John
+m.render(document.body, 'what fuck');
